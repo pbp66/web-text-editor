270d252a24a589b3b1881005dc1e669bfdb09e4e
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-01-31 22:07:45 -0500

    added content to config.js, added post and get logic for the database, and updated app install logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,42 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+	openDB("jate", 1, {
+		upgrade(db) {
+			if (db.objectStoreNames.contains("jate")) {
+				console.log("jate database already exists");
+				return;
+			}
+			db.createObjectStore("jate", {
+				keyPath: "id",
+				autoIncrement: true,
+			});
+			console.log("jate database created");
+		},
+	});
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+	console.log("Saving text editor to database");
+
+	const contactDb = await openDB("editor", 1);
+	const newTransaction = contactDb.transaction("editor", "readwrite");
+	const objectStore = newTransaction.objectStore("editor");
+	const request = objectStore.add({ text: content });
+	const result = await request;
+	console.log("text saved!", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+	console.log("Retrieving previous text editor content");
+	const contactDb = await openDB("editor", 1);
+	const newTransaction = contactDb.transaction("editor", "readonly");
+	const objectStore = newTransaction.objectStore("editor");
+	const request = objectStore.getAll();
+	const result = await request;
+	console.log("result.value", result);
+	return result;
+};
 
 initdb();
